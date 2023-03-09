31ccf9c442a0222d3c85dd32fad22199df9cd28f
Hyuk Kwon <poiu03290@naver.com>
2023-03-10 00:30:14 +0900

    first commit

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,7 +1,19 @@
-let todoItems: object[];
+// type Todo = {
+//   id: number;
+//   title: string;
+//   done: boolean;
+// }
+
+interface Todo {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+let todoItems: Todo[];
 
 // api
-function fetchTodoItems(): object[] {
+function fetchTodoItems(): Todo[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -16,7 +28,7 @@ function fetchTodos(): object[] {
   return todos;
 }
 
-function addTodo(todo: object): void {
+function addTodo(todo: Todo): void {
   todoItems.push(todo);
 }
 
@@ -24,7 +36,7 @@ function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index: number, todo: any): void {
+function completeTodo(index: number, todo: Todo): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
@@ -34,8 +46,8 @@ function logFirstTodo(): object {
   return todoItems[0];
 }
 
-function showCompleted() {
-  // return todoItems.filter(item => item.done);
+function showCompleted(): object {
+  return todoItems.filter(item => item.done);
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
