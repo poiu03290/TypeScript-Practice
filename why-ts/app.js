349e336a2a03041fdbf2f11185bbb6d86fbd100d
Hyuk Kwon <poiu03290@naver.com>
2023-03-10 00:17:33 +0900

    first commit

--- why-ts/app.js
+++ why-ts/app.js
@@ -9,12 +9,32 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {string} address
+ */
+
+/**
+ * 
+ * @returns {Promise<User>}
+ */
+
+const fetchUser = () => {
+  return axios.get(url)
+}
+
 function startApp() {
-  axios
-    .get(url)
+  // axios
+  //   .get(url)
+  fetchUser
     .then(function (response) {
       console.log(response);
       user = response.data;
+      username.innerHTML = user[0].name;
+      email.innerHTML = user[0].email;
+      address.innerHTML = user[0].address.street;
       // TODO: 이름, 이메일, 주소 표시하기
     })
     .catch(function (error) {
