5a90eaeb6a1fc75b1bdae388f1c5281025a54aa9
Hyuk Kwon <poiu03290@naver.com>
2023-03-23 00:38:51 +0900

    type example

---
+++ class-note/10_type-assertion.ts
@@ -0,0 +1,10 @@
+// 타입 단언(type assertion)
+let n;
+n = 20;
+n = 'a'
+let b = n as string;
+
+// DOM API 조작
+// <div class='app'>hi</div>
+
+const div = document.querySelector('.hi');
---
+++ class-note/12_type-compatibility.ts
@@ -0,0 +1,13 @@
+// 인터페이스
+interface Developer {
+    name: string;
+    skill: string;
+}
+
+class Person {
+    name: string;
+}
+
+let developer: Developer;
+let person: Person;
+// developer = person;
